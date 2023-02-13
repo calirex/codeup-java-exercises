ce85ce434a617c11873564a9b9400d15177310d8
Fernando Rivera <fernando.rivera0011@gmail.com>
2023-02-13 09:55:04 -0600

    Completed String Exercise

---
+++ src/StringExercise.java
@@ -0,0 +1,22 @@
+public class StringExercise {
+    public static void main(String[] args) {
+        // 1. //
+        String education = "We don't need no education";
+        System.out.println(education.replace("education", "thought control"));
+
+        // 2. //
+        String quotes = "Check \"this\" out!, \"s inside of \"s!";
+        System.out.println(quotes);
+
+        // 3. //
+        String windows = "In windows, the main drive is usually C:\\";
+        System.out.println(windows);
+
+        // 4. //
+        String backSlash = "I can do backslashes \\, double backslashes \\\\ and the amazing triple Backslash \\\\\\ ";
+        System.out.println(backSlash);
+
+
+    }
+
+}
