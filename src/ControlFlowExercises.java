adc62c14f11cf3b95743254a0cb71784a333994c
Fernando Rivera <fernando.rivera0011@gmail.com>
2023-02-14 16:32:34 -0600

    Completed Exercises

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class ControlFlowExercises {
 
     public static void main(String[] args) {
@@ -28,6 +30,65 @@ public class ControlFlowExercises {
 
 //        for(int i = 5; i <= 15; i += 1){
 //            System.out.println(i);
+//        }
+//        for (int i = 0; i <= 100; i += 2) {
+//            System.out.println(i);
+//        }
+//
+//        for (int i = 100; i >= 5; i -= 5) {
+//            System.out.println(i);
+//        }
+//
+//        for (long i = 2; i <= 1000000; i *= i) {
+//            System.out.println(i);
+//        }
+//
+//        for (int i = 0; i <= 100; i += 1){
+//            if( i % 3 == 0 && i % 5 == 0){
+//                System.out.println("fizzbuzz");
+//            } else if (i % 3 == 0){
+//                System.out.println("fizz");
+//            } else if (i % 5 == 0){
+//                System.out.println("buzz");
+//            } else {
+//                System.out.println(i);
+//            }
+//        }
+//
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("Pick a number.");
+//        int Num = sc.nextInt();
+//        System.out.println("Do you want to continue: [y/N]");
+//        String continueY = sc.next();
+//        if (continueY.equals("y")){
+//            System.out.println("""
+//            Here is a table!
+//
+//            number | squared | cubed
+//            ------ | ------- | -----
+//            """);
+//            for (int i = 0; i <= 5; i += 1) {
+//                System.out.println(i + "      |" + i * Num + "       |" + i * Num * Num);
+//            }
+//
+//        } else {
+//        }
+//
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("Enter a number.");
+//        int grade = sc.nextInt();
+//        System.out.println("Do you want to continue: [y/N]");
+//        String continueY = sc.next();
+//        if(continueY.equals("y") && grade <= 59){
+//            System.out.println("F: 59 - 0");
+//        } else if(continueY.equals("y") && grade >=  60 && grade <= 66){
+//            System.out.println("D: 66-60");
+//        } else if(continueY.equals("y") && grade >=  67 && grade <= 79){
+//            System.out.println("C: 79-67");
+//        } else if(continueY.equals("y") && grade >=  80 && grade <= 87){
+//            System.out.println("B: 87-80");
+//        } else if(continueY.equals("y") && grade >=  88 && grade <= 100){
+//            System.out.println("A: 100-88");
 //        }
 
 
---
+++ src/OopLecture.java
@@ -0,0 +1,194 @@
+public class OopLecture {
+    public static void main(String[] args) {
+
+//        -- OOP SHORT ASSIGNMENT #1 --
+//        Create a class called Dish. This class will represent various restaurant dishes.
+//        Include an integer property called “costInCents”
+//        Include a string property called “nameOfDish”
+//        Include a boolean property called “wouldRecommend”
+//        Include a method called “printSummary” with a void return type
+//        this method will use a single printf method to print out the values of each of the instance properties in the following format:
+//        Cost: COST_IN_CENTS_HERE
+//        Name: NAME_OF_DISH_HERE
+//        Recommended: BOOLEAN_VALUE_OF_WOULD_RECOMMEND_HERE
+//        Create another class called DishTest
+//                Add a main method and inside the method...
+//        1) instantiate a Dish object and assign to a variable called dish1
+//        2) assign creative values for each of the properties
+//        3) test the printSummary() method by invoking it and checking if all instance values are correctly printed
+//
+
+
+
+
+
+//        SHORT ASSIGNMENT #2 //
+//        Create a class of static members (variables and methods) called DishTools
+//        - AVERAGE_COST_OF_DISH_IN_CENTS - an integer constant set to 1300
+//                - shoutDishName() - that takes in a Dish object and prints out the name in all caps
+//                - analyzeDishCost() - that takes in a Dish object and will print out either “More expensive than average” or “Less expensive than average”, depending on the value of the dish costInCents compared to AVERAGE_COST_OF_DISH_IN_CENTS
+//                - flipRecommendation() - that takes in a Dish object and reverses the wouldRecommend boolean value
+//        Try out the DishTools methods with Dish objects in the DishTest main method
+//        BONUS - add some static methods to the Dish class that compares two dishes in some way
+
+
+
+
+
+
+
+
+//package oop;
+
+import java.util.Date;
+
+        public class Post {
+
+    /*
+      int id
+      String title
+      String content
+      String author
+      Date created_at
+     */
+
+            public int id;
+            public String title;
+            public String content;
+            public String author;
+            public Date created_at;
+
+            public String returnAuthorDateMessage() {
+                return String.format("This post was created by %s on %s", author, created_at);
+            }
+
+//    @Override
+//    public String toString() {
+//        return "Post{" +
+//            "id=" + id +
+//            ", title='" + title + '\'' +
+//            ", content='" + content + '\'' +
+//            ", author='" + author + '\'' +
+//            ", created_at=" + created_at +
+//            '}';
+//    }
+
+
+
+            public static void main(String[] args) {
+                Post post1 = new Post();
+                post1.id = 1;
+                post1.title = "Day in the Life";
+                post1.content = "It happens every day...";
+                post1.author = "Overlord";
+                post1.created_at = new Date();
+
+                Post post2 = new Post();
+                post2.id = 2;
+                post2.title = "A Day";
+                post2.content = "It happens...";
+                post2.author = "Mr. Overlord";
+                post2.created_at = new Date();
+
+
+                System.out.println(post1.returnAuthorDateMessage());
+                System.out.println(post2.returnAuthorDateMessage());
+
+//        System.out.println(post1);
+//
+//        System.out.println(post1.title);
+//        Post p = post1;
+//        p.title = "LOL";
+//        System.out.println(post1.title);
+//
+//
+//        System.out.println(p);
+
+//        System.out.println(post2);
+
+            }
+
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//        ================================= OOP INTRO
+
+//        -- UP TO NOW --
+//
+//                - using functions/methods to group together steps for a program to work
+//
+//
+//        -- DEFINITION --
+//
+//        OOP - Object-oriented programming
+//
+//                - A programming paradigm where state (data/properties) and behavior (functionality/methods)
+//        are grouped together (often into objects)
+//                - Objects in Java are created from classes (a blueprint for an object/instance)
+//
+//
+//        -- ADVANTAGES OF OOP --
+//
+//                Programming in an Object-oriented way can be used to develop code with a clearer and more modular organization in the following ways:
+//
+//        - implementation details can be abstracted away making code more readable (Abstraction)
+//                - restriction of access to various levels of abstraction preventing data inconsistencies (Encapsulation)
+//                - duplication is minimized through inheritance hierarchies (Inheritance)
+//                - flexible code where multiple implementations may be swapped out behind a common interface (Polymorphism)
+//
+//
+//                -- THE FOUR PILLARS OF OOP --
+//
+//        1. Abstraction - keep it clean
+//        2. Encapsulation - keep it contained
+//        3. Inheritance - keep it DRY
+//        4. Polymorphism - keep it versatile
+//
+//        https://medium.com/@hamzzza.ahmed95/four-pillars-of-object-oriented-programming-oop-e8d7822aa219
+//
+//        Today we will focus on Abstraction and Encapsulation.
+//
+//
+//        -- CONCEPTS COVERED --
+//
+//                - how to create custom classes and objects from these classes
+//                - defining and using instance and static fields
+//        - how to control class constructors
+//        - using the "this" keyword
+//                - controlling encapsulation with visibility modifiers
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
